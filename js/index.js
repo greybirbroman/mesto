f5ed3d55121339ebf3297c5c541bd2c326d4955d
Roman Fedorov <rfedorov.nagash@gmail.com>
2022-04-08 17:18:11 +0300

    add like/delite button js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,81 @@
+// CONTENT CARDS
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+// Получить карточку из JS
+const cardsList = document.querySelector('.elements__list');
+const template = document.querySelector('.template-element');
+
+
+function render() {
+  const html = initialCards.map(getElement);
+  cardsList.append(...html);
+}
+
+function getElement(item) {
+
+  const cardElement = template.content.cloneNode(true);
+  const cardImage = cardElement.querySelector('.element__image');
+  const cardName = cardElement.querySelector('.element__name');
+  const cardLikeBtn = cardElement.querySelector('.element__like-button');
+  const cardDeliteBtn = cardElement.querySelector('.element__delite-button');
+  cardName.textContent = item.name;
+  cardImage.src = item.link;
+
+  // Добавить Like
+
+  cardLikeBtn.addEventListener('click', () => {
+    cardLikeBtn.classList.toggle('element__like-button_type_active');
+  });
+
+  // Удалить Карточку
+
+  cardDeliteBtn.addEventListener('click', () => {
+    cardDeliteBtn.closest('.element').remove();
+  });
+  return cardElement;
+}
+render();
+
+
 // ПЕРЕМЕННЫЕ
 
-let editButton = document.querySelector('.profile__edit-button'); // Кнопка редактирования профиля
-let popup = document.querySelector('.popup'); // Попап
-let popupCloseButton = document.querySelector('.popup__close'); // Кнопка закрытия попапа
-let formElement = document.querySelector('.popup__form'); // Форма попапа
-let nameInput = formElement.querySelector('#name'); // Инпут Name
-let jobInput = formElement.querySelector('#job'); // Инпут Job
-let profileName = document.querySelector('.profile__name'); // Имя Профиля
-let profileActivity = document.querySelector('.profile__activity'); // Должность Профиля
+const editButton = document.querySelector('.profile__edit-button'); // Кнопка редактирования профиля
+const popup = document.querySelector('.popup'); // Попап
+const popupCloseButton = document.querySelector('.popup__close'); // Кнопка закрытия попапа
+const formElement = document.querySelector('.popup__form'); // Форма попапа
+const nameInput = formElement.querySelector('#name'); // Инпут Name
+const jobInput = formElement.querySelector('#job'); // Инпут Job
+const profileName = document.querySelector('.profile__name'); // Имя Профиля
+const profileActivity = document.querySelector('.profile__activity'); // Должность Профиля
 
-// POPUP OPEN FUNCTION
+
+// EDIT_PROFILE POPUP OPEN FUNCTION
 
 function openPopup(evt) {
   popup.classList.add('popup_open');
@@ -17,13 +83,13 @@ function openPopup(evt) {
   jobInput.value = profileActivity.textContent;
 }
 
-// POPUP CLOSE FUNCTION
+// EDIT_PROFILE POPUP CLOSE FUNCTION
 
 function closePopup() {
   popup.classList.remove('popup_open');
 }
 
-// POPUP SUBMIT FUNCTION
+// EDIT_PROFILE POPUP SUBMIT FUNCTION
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
@@ -36,3 +102,4 @@ function formSubmitHandler(evt) {
 editButton.addEventListener('click', openPopup); // Прослушиватель на *edit-button*
 popupCloseButton.addEventListener('click', closePopup); // Прослушиватель *на close-button*
 formElement.addEventListener('submit', formSubmitHandler); // Прослушиватель *на submit-button*
+
