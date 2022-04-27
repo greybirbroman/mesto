42d606730849b9ad9004866121602df69081399d
Roman Fedorov <rfedorov.nagash@gmail.com>
2022-04-27 16:20:58 +0300

    test new code2

--- js/index.js
+++ js/index.js
@@ -1,31 +1,4 @@
-// CONTENT CARDS
 
-const initialCards = [
-  {
-    name: 'Дуайт',
-    link: 'https://kinoblog.tv/images/films/365.jpg'
-  },
-  {
-    name: 'Хеллоуин',
-    link: 'https://purewows3.imgix.net/images/articles/2019_08/the_office_halloween_episodes_gabe_and_ryan.jpg?auto=format,compress&cs=strip'
-  },
-  {
-    name: 'Джим и Пэм',
-    link: 'https://xage.ru/media/posts/2021/5/29/semka-odnoj-stseny-v-seriale-ofis-oboshlas-v-250-tysjach-dollarov.jpg'
-  },
-  {
-    name: 'Стенли доволен?',
-    link: 'https://www.indiewire.com/wp-content/uploads/2020/07/Screen-Shot-2020-07-06-at-10.39.06-AM.png'
-  },
-  {
-    name: 'Холли',
-    link: 'https://gol.ru/images/original/materials/sections/82278/82278.jfif'
-  },
-  {
-    name: 'Dunder Mifflin',
-    link: 'https://avatars.mds.yandex.net/get-kinopoisk-post-img/1528730/bea07e06cd2826fbf4784a3069b4e975/960'
-  }
-];
 
 // POPUP
 const popups = document.querySelectorAll('.popup'); // Popups
